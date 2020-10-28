5ce176db54643b01d3a59bdba6b1c58f8472fdf8
ibadeeCodes <ibadeeshaikh@gmail.com>
2020-10-28 07:08:44 -0700

    changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '/public')))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
+  app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')) // relative path
   })
 }
