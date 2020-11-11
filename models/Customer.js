5983e4bcf02b52a98c8e2fccac0384a48490da3f
ibadeeCodes <ibadeeshaikh@gmail.com>
2020-11-11 10:00:42 -0800

    done with user module

--- models/Customer.js
+++ models/Customer.js
@@ -1,30 +1,43 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const CutomerSchema = new mongoose.Schema({
-    name: {
-        type: String,
-    },
-    contactnumber: {
-        type: String,
-    },
-    address: {
-        type: String,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    noOfOrders: {
-        type: String,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+  name: {
+    type: String,
+  },
+  contactnumber: {
+    type: String,
+  },
+  online_account: {
+    type: String,
+  },
+  membership: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  company: {
+    type: String,
+  },
+  company_address: {
+    type: String,
+  },
+  birthday: {
+    type: Date,
+  },
+  noOfOrders: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
 
-},
-);
-
-module.exports = Customer = mongoose.model("customer", CutomerSchema);
+module.exports = Customer = mongoose.model('customer', CutomerSchema)
